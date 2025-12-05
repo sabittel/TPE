91296f152df17bb9aa32ee960fc895f0c62a560a
“Santiago <sabittel@itba.edu.ar>
2025-12-05 00:26:07 +0000

    estructuras basicas

--- backend/servicesADT.c
+++ backend/servicesADT.c
@@ -1,5 +1,44 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "servicesADT.h"
 
-typedef struct servicesCDT{
-}servicesCDT;
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+
+typedef struct typeNode { /*por cada linea del Types.csv creo un nodo (es como un diccionario)*/
+    char *code; /*codigo*/
+    char *name; /*nombre real*/
+    struct typeNode *next;
+} typeNode;
+
+typedef struct typeCountNode { /*cuantas quejas hay por tipo en el barrio (Query 1)*/
+    char *typeCode; /*tipo*/
+    size_t count; /*contador*/
+    struct typeCountNode *next;
+} typeCountNode;
+
+typedef struct boroughNode {
+    char *borough; /*nombre del barrio*/
+    typeCountNode *types;      /* para Query 1 */
+    size_t closedByHour[24];   /* para Query 2, cantidad de reclamos cerrados en este barrio a las n horas */
+    struct boroughNode *next;
+} boroughNode;
+
+typedef struct typeListNode { /*lista de tipos usados por cuadrante*/
+    char *typeCode;            /* código de tipo (distinto) */
+    struct typeListNode *next;
+} typeListNode;
+
+typedef struct quadrantNode { /*representa un cuadrante*/
+    int quadLat;
+    int quadLon;
+    typeListNode *openTypes;   /* lista de tipos abiertos distintos */
+    struct quadrantNode *next;
+} quadrantNode;
+
+struct cityServicesCDT { 
+    typeNode    *types;       /* diccionario code -> name */
+    boroughNode *boroughs;    /* estructuras para Q1 y Q2 */
+    quadrantNode *quadrants;  /* estructuras para Q3 */
+};
+
--- backend/servicesADT.h
+++ backend/servicesADT.h
@@ -1,1 +1,9 @@
-typedef struct servicesADT * servicesCDT;
+#ifndef CITY_SERVICES_ADT_H
+#define CITY_SERVICES_ADT_H
+
+
+//TAD opaco
+typedef struct cityservicesCDT *cityservicesADT;
+
+
+#endif
