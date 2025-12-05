ae651cffead66ac813a2e7b282328dd29296f097
“Santiago <sabittel@itba.edu.ar>
2025-12-05 22:31:13 +0000

    Verifico csv

--- frontend/cityServicesNYC.c
+++ frontend/cityServicesNYC.c
@@ -5,10 +5,19 @@
 
 #include "../backend/servicesADT.h"
 
+int hasCSVExtension(const char *path);
+
 int main(){
     if(){ /*validar que se pasen los csv*/
         .
     }
     cityServicesADT service = newServiceADT(NYC_archivo); /*creo el TAD para NYC*/
     .
+}
+
+int hasCSVExtension(const char *path) {
+    const char *dot = strrchr(path, '.');
+    if (dot == NULL)
+        return 0;
+    return strcmp(dot, ".csv") == 0;
 }
